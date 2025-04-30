d7a50fa82beba5496b87dc834902430449f1cf53
hudefao <hudefaosman@gmail.com>
2025-04-30 17:45:22 -0500

    Update Main.c

--- Main.c
+++ Main.c
@@ -12,9 +12,9 @@
 #include "xc.h"
 #include <stdlib.h> // for rand
 #include "iLEDasm.h"
-#include "iLEDwriteColor.h"
-#include "bluetooth.h"
-// UART parameters for TimeLcd board
+#include "iLEDwriteColorr.h"
+// #include "bluetooth.h"   // <-- Bluetooth include removed
+
 #define FREQ    16000000UL
 #define BAUD    38400
 
@@ -28,36 +28,25 @@
 #define BUTTON PORTBbits.RB7
 #define DEBOUNCE_DELAY 50
 
+/*
+void uart_init(void) {
+    TRISBbits.TRISB7 = 1;   // U1RX as input
+    TRISBbits.TRISB6 = 0;   // U1TX as output
+    U1BRG = (unsigned int)((FREQ/(16UL*BAUD)) - 1);
+    U1MODEbits.UARTEN = 1;  // enable UART
+    U1STAbits.UTXEN  = 1;    // enable TX
+}
 
-//void uart_init(void) {
-//    TRISBbits.TRISB7 = 1;   // U1RX as input
-//    TRISBbits.TRISB6 = 0;   // U1TX as output
-//    U1BRG = (unsigned int)((FREQ/(16UL*BAUD)) - 1);
-//    U1MODEbits.UARTEN = 1;  // enable UART
-//    U1STAbits.UTXEN  = 1;    // enable TX
-//}
+void sendData(const char *s) {
+    while (*s) {
+        while (U1STAbits.UTXBF); 
+        U1TXREG = *s++;
+    }
+    // terminate line
+    while (U1STAbits.UTXBF); U1TXREG = '\r';
+    while (U1STAbits.UTXBF); U1TXREG = '\n';
+}
 
-//void sendData(const char *s) {
-//    while (*s) {
-//        while (U1STAbits.UTXBF); 
-//        U1TXREG = *s++;
-//    }
-//    // terminate line
-//    while (U1STAbits.UTXBF); U1TXREG = '\r';
-//    while (U1STAbits.UTXBF); U1TXREG = '\n';
-//}
-//void readString(char *buffer, int maxLength) {
-//    int i = 0;
-//    char c;
-//
-//    do {
-//        while (!U1STAbits.URXDA); // Wait for a new character
-//        c = U1RXREG;              // Read received character
-//        buffer[i++] = c;          // Store it in buffer
-//    } while (c != '\n' && i < (maxLength - 1)); // Stop on newline or buffer full
-//
-//    buffer[i] = '\0'; // Null-terminate the string
-//}
 void readString(char *buffer, int maxLength) {
     int i = 0;
     char c;
@@ -79,8 +68,7 @@ void readString(char *buffer, int maxLength) {
 
     buffer[i] = '\0'; // Null terminate
 }
-
-
+*/
 
 void delay_ms(unsigned int ms) {
     while (ms-- > 0) {
@@ -89,49 +77,33 @@ void delay_ms(unsigned int ms) {
     }
 }
 
-// === DELAY WRAPPER ===
-//void delay(int delay_in_ms) {
-//    int i;
-//    for (i = 0; i < delay_in_ms; i++) {
-//        ETO_wait_1ms();
-//    }
-//}
-
 void write_color(int r, int g, int b) {
     int i;
     
-   
-    //red
     for (i = 7; i >= 0; i--) {
-        if ((r & (1 << i)) > 0)
-            write_1();    
-        else
-            write_0();        
+        if ((r & (1 << i)) > 0) write_1();    
+        else write_0();        
     }
    
-    //green
-    for ( i = 7; i >= 0; i--) {
-       if ((g & (1 << i)) > 0) write_1();          
-       else write_0();      
+    for (i = 7; i >= 0; i--) {
+        if ((g & (1 << i)) > 0) write_1();          
+        else write_0();      
     }  
-    //blue
+
     for (i = 7; i >= 0; i--) {
         if ((b & (1 << i)) > 0) write_1();    
-        else    write_0();    
+        else write_0();    
     }  
    
     ETO_wait_100us();
-     
 }
 
-// === LCD & GAME SETUP ===
 void setup(void) {
     CLKDIVbits.RCDIV = 0;
     AD1PCFG = 0xFFFF;
 
-    TRISA = 0b1111111111111110; // RA0 = output (bit 0 = 0), all others inputs
-  // RA0 = output (LED)
-    TRISB = 0b10000000;          // RB7 = input (button)
+    TRISA = 0b1111111111111110; // RA0 = output
+    TRISB = 0b10000000;         // RB7 = input (button)
     LATA = 0x0000;
     LATB = 0x0000;
 
@@ -148,15 +120,13 @@ void setup(void) {
 }
 
 void waitForButtonPress(void) {
-    // Wait for the button to be pressed
     while (BUTTON == 1);
-    LATA = 0x0001;  // Turn on an LED (RA0) for feedback
+    LATA = 0x0001;
     delay_ms(DEBOUNCE_DELAY);
 
-    // Wait for the button to be released
     while (BUTTON == 0);
     delay_ms(DEBOUNCE_DELAY);
-    LATA = 0x0000;  // Turn off the LED
+    LATA = 0x0000;
 }
 
 void lcd_cmd(char package) {
@@ -217,10 +187,11 @@ void lcd_printStr(const char *str) {
         lcd_printChar(*str++);
     }
 }
-//TRISB = "AT+ROLE=1\r\n";
+
 void clear(void) {
     lcd_cmd(0x01);
 }
+
 void scrollText(const char *str, int delay_per_step, int repeatTime_ms) {
     int len = 0;
     while (str[len] != '\0') len++;
@@ -246,94 +217,80 @@ void scrollText(const char *str, int delay_per_step, int repeatTime_ms) {
         delay_ms(delay_per_step);
     }
 }
+
 int main(void) {
-//    initUart();
-//    delay(1000);
-//
-//    // Try sending one simple character 'A'
-//    while (U1STAbits.UTXBF);  // Wait if TX buffer full
-//    U1TXREG = 'A';            // Load 'A' to transmit register
-//    while (U1STAbits.TRMT == 0); // Wait until transmission complete
-//
-//    while (1);
-    setup();
-    initLCD();
-    char response[100];  // Buffer to hold incoming message
-    initUart();
-    delay(5);
-    // after we send a AT Command there should be a response we can check the response with readString function
-    int randomColor;
-    //setup for the HC-05 aka master will have to have a different file to set up the slave 
-    sendData("AT+NAMEHC-05");
-    readString(response,sizeof(response));
-    
-    //check response variable here when debugging should be okHC-05
-    sendData("AT+PE"); //sets even parity bit 
-    readString(response,sizeof(response));
-    sendData("AT+ROLE=1");          // Set as master
-    readString(response,sizeof(response));
-    sendData("AT+CMODE=0");         // Connect to specific address
-    readString(response,sizeof(response));
-    sendData("AT+BIND=XXXX,YY,ZZZZZZ"); // Bind to HC-06?s MAC address this address will be found by using the sendData("AT+ADDR?");readString(response, sizeof(response)); on the HC-06 
-    readString(response,sizeof(response));
-    scrollText("Press to start", 250, 3000);
-    waitForButtonPress();
+    while (1) {  
+        setup();
+        initLCD();
 
-    scrollText("Only press when light is GREEN", 200, 10000);
+        scrollText("Press to start", 250, 3000);
+        waitForButtonPress();
 
-    for (int round = 1; round <= 5; round++) {
-        clear();
-        lcd_setCursor(0, 0);
-        lcd_printStr("Round ");
-        lcd_printChar('0' + round);
-        delay_ms(5000);
-
-        clear();
-        lcd_setCursor(0, 0);
+        scrollText("Press on GREEN", 200, 5000);
+        
         lcd_printStr("Get ready...");
         delay_ms(1500);
 
-        int waitTime = 2000 + rand() % 6001;
-        delay_ms(waitTime);
-        
-        //while(!g)
-        randomColor = 1 + rand() % (3-1+1);
-        switch(randomColor){
-            case 1:
-                writeColor(255,0,0);
-                break;
-            case 2:
-                //  g=1;
-                writeColor(0,255,0);
-                // add input capture stuff here as we want to only start the ic timer and all that when the light turns green 
-                //so have to add all that like ic1setup(); and then a while statment to wait until they click and then save that data abd orubt ut )
+        int greenRoundsCompleted = 0;  // Track completed green rounds
+
+        while (greenRoundsCompleted < 5) {  // Only proceed until 5 green rounds are completed
+
+            clear();
+            lcd_setCursor(0, 0);
+
+            // Random wait time between 2000 and 8000 ms before showing the light
+            int waitTime = 2000 + rand() % 6001;  
+
+            int randomColor = 1 + rand() % 2;  // Randomly choose red (1) or green (2)
+
+            if (randomColor == 1) {
+                // RED light
+                delay_ms(waitTime);
+                writeColor(255, 0, 0);  // Turn on red light
+                int duration = 2000;  // 2 seconds watching time
+                while (duration > 0) {
+                    if (BUTTON == 0) {  // Pressed during red
+                        writeColor(0, 0, 0);
+                        clear();
+                        lcd_setCursor(0, 0);
+                        lcd_printStr("You failed!");
+                        lcd_setCursor(1, 0);
+                        lcd_printStr("Game over.");
+                        delay_ms(3000);
+                        goto restartGame;  // Restart game from beginning
+                    }
+                    delay_ms(10);
+                    duration -= 10;
+                }
+                writeColor(0, 0, 0);  // Turn off red light
+            } else if (randomColor == 2) {
+                // GREEN light
                 clear();
                 lcd_setCursor(0, 0);
+
+                delay_ms(waitTime);  // Wait before showing green light
+
                 lcd_printStr("GO!");
-                write_color(0, 255, 0); // Green LED
+                writeColor(0, 255, 0);  // Turn on green light
 
-                waitForButtonPress();
-                write_color(0, 0, 0);   // Turn off LED
+                waitForButtonPress();  // Wait for button press to finish round
 
+                writeColor(0, 0, 0);  // Turn off LED
                 clear();
                 lcd_setCursor(1, 0);
-                lcd_printStr("Nice!");
-                delay_ms(1500);
-                break;
-            case 3:
-                writeColor(0,0,255);
-                break;
-        
-        }
-        
-    
 
-    clear();
-    lcd_setCursor(0, 0);
-    lcd_printStr("All done!");
-    delay_ms(3000);
+                greenRoundsCompleted++;  // Increment green round counter
+            }
+        }
+
+        clear();
+        lcd_setCursor(0, 0);
+        lcd_printStr("All done!");
+        delay_ms(3000);
+
+    restartGame:
+        ;
     }
 
-    
     return 0;
 }
